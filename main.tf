86b1f502cb1cb6e3ae45ba60ed02d3fffaf79f9e
lucnietox <luciano.nieto@gmail.com>
2023-07-05 16:24:36 +0000

    modified resources

--- main.tf
+++ main.tf
@@ -24,3 +24,15 @@ resource "azurerm_data_factory" "adf-dev" {
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 }  
+
+
+resource "azurerm_storage_account" "adb-dev" {
+    name                     = "adbdevidlnxp"
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+    account_kind             = "StorageV2"
+    is_hns_enabled           = "true"
+}
+
